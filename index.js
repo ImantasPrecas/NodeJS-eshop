1b14caf35c0c42055829ef68c934fc0b088431b0
Imantas <precasimantas@gmail.com>
2022-03-21 14:49:00 +0200

    task

---
+++ index.js
@@ -0,0 +1,22 @@
+/*
+Uztuotis:
+
+-perskaityti visu produktu failus
+-susideti visus produktus i viena masyva
+-isspausdinti produktu lentele, kuri atrodo taip:
+
+"Univermagas" pardavime turi:
+-----------------------------
+1) Preke pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
+2) Preke pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
+3) Preke pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
+-----------------------------
+Parduotuves suvestine:
+- turimu prekiu sandelyje: [total kiekis]
+- parduotu prekiu: [total kiekis]
+- suprekiauta suma: [total pinigu] [valiuta]
+- galimu pardavimu: [total pinigu] [valiuta]
+- maksimali galima parduotuves apyvarta: [total pinigu] [valiuta]
+
+
+*/
