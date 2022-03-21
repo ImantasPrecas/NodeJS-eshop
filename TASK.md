d04ae301d3c6a4301e00220dde27e9469380b55c
Imantas <precasimantas@gmail.com>
2022-03-22 00:05:34 +0200

    PrintList

---
+++ TASK.md
@@ -0,0 +1,21 @@
+
+# Uztuotis:
+
+- perskaityti visu produktu failus
+- susideti visus produktus i viena masyva
+- isspausdinti produktu lentele, kuri atrodo taip:
+
+```bash
+"Univermagas" pardavime turi:
+-----------------------------
+1) Preke pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
+2) Preke pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
+3) Preke pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
+-----------------------------
+Parduotuves suvestine:
+- turimu prekiu sandelyje: [total kiekis]
+- parduotu prekiu: [total kiekis]
+- suprekiauta suma: [total pinigu] [valiuta]
+- galimu pardavimu: [total pinigu] [valiuta]
+- maksimali galima parduotuves apyvarta: [total pinigu] [valiuta]
+```
--- index.js
+++ index.js
@@ -1,27 +1,46 @@
-const sum = require("./math/sum.js");
+const fs = require("fs/promises");
+const path = require("path");
+const jsonParse = require("./lib/jsonParse.js");
+const printList = require("./lib/printList.js");
+const readFile = require("./lib/readFile.js");
 
-/*
-Uztuotis:
+(async () => {
+	const goods = [
+		"arbata",
+		"kvepalai",
+		"masina",
+		"masina-wrong-1",
+		"masina-wrong-2",
+		"masina-wrong-3",
+		"arba",
+		"pomidoras",
+		"",
+		5,
+		true,
+		false,
+		null,
+	];
 
--perskaityti visu produktu failus
--susideti visus produktus i viena masyva
--isspausdinti produktu lentele, kuri atrodo taip:
+	const goodsInfo = [];
 
-"Univermagas" pardavime turi:
------------------------------
-1) Preke pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
-2) Preke pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
-3) Preke pavadinimas: [kaina] [valiuta]; parduota: [kiekis]; likutis: [kiekis];
------------------------------
-Parduotuves suvestine:
-- turimu prekiu sandelyje: [total kiekis]
-- parduotu prekiu: [total kiekis]
-- suprekiauta suma: [total pinigu] [valiuta]
-- galimu pardavimu: [total pinigu] [valiuta]
-- maksimali galima parduotuves apyvarta: [total pinigu] [valiuta]
+	for (const item of goods) {
+		//gateKeeperiai
+		if (typeof item !== "string" || item === "") {
+			continue;
+		}
+		const itemText = await readFile(item);
+		if (typeof itemText !== "string" || itemText === "") {
+			continue;
+		}
+		const itemObj = jsonParse(itemText);
+		if (itemObj === false) {
+			continue;
+		}
+		goodsInfo.push(itemObj);
+	}
+	console.log('"Univermagas" pardavime turi:');
+	console.log("------------------------------");
+	console.log(printList(goodsInfo));
 
-
-*/
-
-const rez = sum(5, 7);
-console.log(rez);
+	console.log("------------------------------");
+})();
