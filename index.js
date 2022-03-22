f025145d9b2e3e836b10308fc2730ae2db65aa71
Imantas <precasimantas@gmail.com>
2022-03-22 23:42:04 +0200

    More Validations

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const fs = require("fs/promises");
-const path = require("path");
 const jsonParse = require("./lib/jsonParse.js");
 const printList = require("./lib/printList.js");
 const readFile = require("./lib/readFile.js");
@@ -7,18 +5,56 @@ const readFile = require("./lib/readFile.js");
 (async () => {
 	const goods = [
 		"arbata",
+		"arba",
 		"kvepalai",
 		"masina",
 		"masina-wrong-1",
 		"masina-wrong-2",
 		"masina-wrong-3",
-		"arba",
+		"masina-wrong-4",
+		"masina-wrong-5",
+		"masina-wrong-6",
+		"masina-wrong-7",
+		"masina-wrong-8",
+		"masina-wrong-9",
+		"masina-wrong-10",
+		"masina-wrong-11",
+		"masina-wrong-12",
+		"masina-wrong-13",
+		"masina-wrong-14",
+		"masina-wrong-15",
+		"masina-wrong-16",
+		"masina-wrong-17",
+		"masina-wrong-18",
+		"masina-wrong-19",
+		"masina-wrong-20",
+		"masina-wrong-21",
+		"masina-wrong-22",
+		"masina-wrong-23",
+		"masina-wrong-24",
+		"masina-wrong-25",
+		"masina-wrong-26",
+		"masina-wrong-27",
+		"masina-wrong-28",
+		"masina-wrong-29",
+		"masina-wrong-30",
 		"pomidoras",
+		"masina-wrong-31",
+		"masina-wrong-32",
+		"masina-wrong-33",
+		"masina-wrong-34",
+		"masina-wrong-35",
+		"masina-wrong-36",
 		"",
 		5,
 		true,
 		false,
 		null,
+		() => {},
+		[],
+		{},
+		undefined,
+		"masina-wrong-37",
 	];
 
 	const goodsInfo = [];
@@ -41,6 +77,7 @@ const readFile = require("./lib/readFile.js");
 	console.log('"Univermagas" pardavime turi:');
 	console.log("------------------------------");
 	console.log(printList(goodsInfo));
+	console.log(printList(goods));
 
 	console.log("------------------------------");
 })();
